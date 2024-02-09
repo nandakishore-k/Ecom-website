924c16edc1422c2bf728ceaab2806d285b66813a
nandakishore-k <nandakishorek2002@gmail.com>
2024-02-09 21:22:57 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ProductComponent } from './pages/product/product.component';
+import { NavbarComponent } from './units/navbar/navbar.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { FooterComponent } from './units/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ProductComponent,NavbarComponent,ContactComponent,FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'ecom';
+  data:any=[]
+
+
+
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ProductComponent } from './pages/product/product.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SingleComponent } from './pages/single/single.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '',component: HomeComponent},
+    {path: 'product',component: ProductComponent},
+    {path: 'contact',component: ContactComponent},
+    {path: 'product/:id',component: SingleComponent},
+    {path: '**',component: ContactComponent},
+    
+];
